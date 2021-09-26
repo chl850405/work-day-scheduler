31094a19e68acb7501f1768e19a876e71851657a
christi marchetti <christimarchetti@gmail.com>
2021-09-26 18:14:39 -0400

    row and columns

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,29 +1,26 @@
-
-//Defines Current Moment
+var currentDate = 'nowformat'
+var format = 'MM-DD-YYYY';
 var now = moment();
-//Defines current Date
-var currentDate = now.format("MM/DD/YYYY");
+console.log(currentDate)
 
-//Current Date
-$("#currentDay").text("Today's Date" + currentDate);
-console.log(Date);
+$("#currentDay").text(moment().format('dddd MMMM Do YYYY,h:mm a'));
 
-//Rows With Displayed Time Event and Save Button
-$(document).ready(function() {
-    for (let i = 9; i < 9; i++) {
-        //Row
-        var row = $(`div data-time={i} id='${i}' class="row"`);
-        //Time Column
-        var time = $('<div class="col-sm2"> <p class="hour">' + formatAmPm{i} + '<p>')
+// $('document').ready(function() {
+//     //Defines Current Moment
+// var now = moment();
+// //Defines current Date
+// var currentDate = moment().format("MM/DD/YYYY");
 
-    }
-//Time Display by Hour
-function formatAmPm() {
-    var ampm = hours >= 12 ? 'am' : 'pm';
-    hours = hours %12;
-    hours = hours ? hours : 12;
-    return hours + ampm
-    
-}
-console.log (time);
-})
+
+//     if (currentDate < now){
+//         $(document).css('past');
+//         console.log(currentDate)
+//     }
+    // for (let i = 9; i < 9; i++) {
+    //     //Row
+    //     var row = $(`div data-time=[i] id='${i}' class="row"`);
+    //     //Time Column
+    //     var time = $('<div class="col-sm2"> <p class="hour">' + formatAmPm[i] + '<p>')
+
+    // }
+// });
