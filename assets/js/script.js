4377d54c092b8ca485f4479a480a9d7f72c2b3ae
christi marchetti <christimarchetti@gmail.com>
2021-09-27 00:34:10 -0400

    textarea and event indication

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,7 +12,12 @@ $('document').ready(function() {
     var schedule = JSON.parse(localStorage.getItem('schedule'));
     localStorage.setItem('schedule', JSON.stringify(schedule));
     
-    
+    //scroll
+    window.scroll({
+        top: 100,
+        left: 100,
+        behavior: 'smooth'
+    });
     //creates 9 rows
     for (let i = 9; i < 18; i++) {
         // //Row
@@ -21,7 +26,7 @@ $('document').ready(function() {
         var time = $('<div class="col-sm-2"> <p class="hour">' + formatAMPM(i) + '</p>');
         // var time = $('<div class="col-sm-2 hour"></div>');
         //Event
-        var task = $('<div class="col-sm-9 textarea"><div>');
+        var task = $('<div class="col-sm-9"><div><label for="exampleFormControlTextarea1"></label><textarea class="form-control"id="exampleFormControlTextarea1" rows="3"></textarea></div>');
 
         var save = $('<div class="col-sm-1 saveBtn"><button class=""><i class="fas fa-lock"></i></button></div>');
 
@@ -36,4 +41,20 @@ $('document').ready(function() {
         hours = hours ? hours : 12;
         return hours + ampm;
     }
+    function colors () {
+    var currentHour = moment().getHour()
+    for (let i = 9; i < 18; i++) {
+    if (before < currentHour) {
+  // selected hour is in the past
+    $(".hour").addClass("past");
+
+    } else if (after< currentHour) {
+    // selected hour is in the future
+    $(".hour").addClass("future");
+
+    } else if ( currentHour === currentHour) {
+    // selected hour is current
+    $(".hour").addClass("present");
+    };
+    }};
 });
